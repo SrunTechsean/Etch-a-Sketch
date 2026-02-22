9802ef5b55091082e39d25bbbc422a00d0b2b675
SrunTechsean <techseansrun@gmail.com>
2026-02-23 00:17:45 +0700

    feat: allow user to change the size of grid

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const MAX_GRID = 100;
-let grid = 16;
-
 const container = document.querySelector(".container");
+const submitButton = document.querySelector("button");
 
+const MAX_GRID = 100;
+let gridValue = 16;
 // Generate a row of pixel 
 function generateRow(rowContainer) {
-    for (let i = 0; i < grid; i++) {
+    for (let i = 0; i < gridValue; i++) {
 
         // Generate each pixel of the row
         const pixel = document.createElement("div");
@@ -17,7 +17,7 @@ function generateRow(rowContainer) {
 
 // Generate full grid 
 function generateGrid() {
-    for (let i = 0; i < grid; i++) {
+    for (let i = 0; i < gridValue; i++) {
         // Generate Row Container
         const rowContainer = document.createElement("div");
         rowContainer.classList.add("row-container");
@@ -33,3 +33,23 @@ generateGrid();
 container.addEventListener("mouseover", (e) => {
     e.target.classList.add("bg-red");
 })
+
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const gridInput = document.querySelector("#gridInput");
+
+    let inputValue = Number(gridInput.value);
+    if (inputValue <= 0) {
+        gridValue = 16;
+    } else if (inputValue > 100) {
+        gridValue = 100;
+    } else {
+        gridValue = inputValue;
+    }
+
+    // Remove Old grid
+    container.innerHTML = "";
+    generateGrid();
+})
+
+
